3a5defc2a0417e58e0d4349ac1fe43a90a4b043b
Shaurya <shauryajha35@gmail.com>
2026-06-19 16:23:00 +0530

    Implement file find-and-replace logic

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::env;
+use std::fs;
+use regex::Regex;
 use text_colorizer::*;
 
-#derive[Debug]
+#[derive(Debug)]
 struct Arguments {
     target: String,
     replacement: String,
@@ -10,12 +12,36 @@ struct Arguments {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let args = parse_args();
+
+    let data = match fs::read_to_string(&args.filename) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to read from file '{}': {:?}",
+                "Error:".red().bold(), args.filename, e);
+            std::process::exit(1);
+        }
+    };
+
+    let replaced_data = match replace(&args.target, &args.replacement, &data) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to replace text: {:?}",
+                "Error:".red().bold(), e);
+            std::process::exit(1);
+        }
+    };
+
+    if let Err(e) = fs::write(&args.output, &replaced_data) {
+        eprintln!("{} failed to write to file '{}': {:?}",
+            "Error:".red().bold(), args.output, e);
+        std::process::exit(1);
+    }
 }
 
-fn print_usage(){
-    eprintln!("{} - Change occurances of one string into another",
-                "quick replace" .green());
+fn print_usage() {
+    eprintln!("{} - Change occurrences of one string into another",
+        "quickreplace".green());
     eprintln!("Usage: quickreplace <target> <replacement> <INPUT> <OUTPUT>");
 }
 
@@ -24,8 +50,8 @@ fn parse_args() -> Arguments {
 
     if args.len() != 4 {
         print_usage();
-        eprintln!("{} wrong num of arguments: expected 4, got {}.", 
-                "Error:" .red().bold().args.len());
+        eprintln!("{} wrong number of arguments: expected 4, got {}.",
+            "Error:".red().bold(), args.len());
         std::process::exit(1);
     }
 
@@ -35,4 +61,9 @@ fn parse_args() -> Arguments {
         filename: args[2].clone(),
         output: args[3].clone(),
     }
+}
+
+fn replace(target: &str, replacement: &str, text: &str) -> Result<String, regex::Error> {
+    let regex = Regex::new(target)?;
+    Ok(regex.replace_all(text, replacement).to_string())
 }
